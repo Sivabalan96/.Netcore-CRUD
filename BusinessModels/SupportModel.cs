c76312b2638d0f5543dec111216f33781817a059
Gopinath Elumalai <gopinath@twilightsoftwares.com>
2021-02-10 14:35:18 +0530

    Initial Commit

---
+++ BusinessModels/SupportModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessModels
+{
+    public class SupportModel
+    {
+        public int Id { get; set; }
+
+        public String Title { get; set; }
+
+        public String Description { get; set; }
+
+        public String Status { get; set; }
+
+        public String Assignee { get; set; }
+
+        public String Priority { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+        public DateTime ClosedDate { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
